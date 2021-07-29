5880aaea3db7acc81c01a9fd753de8307d6c4c67
Gustavo Guimarães <gguimaraes1602@gmail.com>
2021-07-29 08:24:56 -0300

    Add project files.

---
+++ VersionUpdate/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DirSearch($@"D:\Workbench\Viewer\mobileviewer");
+        }
+
+        static void DirSearch(string sDir)
+        {
+            try
+            {
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    foreach (string f in Directory.GetFiles(d))
+                    {
+                        Console.WriteLine(f);
+                        if (f.Contains("AssemblyInfo.cs"))
+                        {
+                            string a = File.ReadAllText(f);
+                            a = a.Replace($@"[assembly: AssemblyTitle(""PortableViewer", $@"[assembly: AssemblyTitle("" ClearCanvas");
+                            File.WriteAllText(f, a);
+                        }
+                    }
+                    DirSearch(d);
+                }
+
+
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+    }
+}
